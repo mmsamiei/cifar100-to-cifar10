1d9751206dc3d0da36ad0ef6d4980a8225074f28
mmsamiei <mohmahsamiei@gmail.com>
2022-01-19 17:04:55 +0330

    add cross entropy loss

---
+++ losses/cross_entropy_loss.py
@@ -0,0 +1,21 @@
+from copyreg import constructor
+from mimetypes import init
+from multiprocessing.pool import INIT
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+
+
+class CrossEntropyLoss(nn.Module):
+    def __init__(self) -> None:
+        super(CrossEntropyLoss, self).__init__()
+        self.criterion = nn.CrossEntropyLoss()
+    
+    def forward(self, input: torch.Tensor, target: torch.Tensor) -> torch.Tensor:
+        """
+        input: tensor in shape (N, C)
+        target: tensor in shape (N)
+        """
+        return self.criterion(input, target)
+
+
