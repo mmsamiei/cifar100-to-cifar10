95fec7a1f148ca3386b24de32a07811319c93901
mmsamiei <mohmahsamiei@gmail.com>
2022-01-20 15:28:54 +0330

    experment

--- experiments/cifar10_to_cifar100.py
+++ experiments/cifar10_to_cifar100.py
@@ -1,3 +1,8 @@
+import sys, os, inspect
+currentdir = os.path.dirname(os.path.abspath(inspect.getfile(inspect.currentframe())))
+parentdir = os.path.dirname(currentdir)
+sys.path.insert(0, parentdir)
+
 from datasets import cifar10_dataset, cifar100_dataset
 import torch
 from models import classifier
@@ -6,24 +11,24 @@ from trainers import supervised_trainer
 from trainers import tester
 from optimizers import optimizers
 
+from torch.utils.data import RandomSampler, DataLoader, Subset
+import numpy as np
+
 device = utils.get_gpu_if_available()
-
 ## build dataset
-cifar10_train_dataset = cifar10_dataset.Cifar10Dataset("train")
+cifar10_train_dataset = Subset(cifar10_dataset.Cifar10Dataset("train"), np.arange(100))
 cifar10_test_dataset = cifar10_dataset.Cifar10Dataset("test")
-cifar100_train_dataset = cifar100_dataset.Cifar100Dataset("train")
+cifar100_train_dataset = Subset(cifar100_dataset.Cifar100Dataset("train"), np.arange(100))
 cifar100_test_dataset = cifar100_dataset.Cifar100Dataset("test")
-
 ## dataloaders
 cifar10_train_dataloader = torch.utils.data.DataLoader(cifar10_train_dataset, \
-    batch_size=128, shuffle=True, num_workers=2)
+    batch_size=16, shuffle=True, num_workers=2)
 cifar10_test_dataloader = torch.utils.data.DataLoader(cifar10_test_dataset, \
-    batch_size=128, shuffle=True, num_workers=2)
+    batch_size=16, shuffle=True, num_workers=2)
 cifar100_train_dataloader = torch.utils.data.DataLoader(cifar100_train_dataset, \
-    batch_size=128, shuffle=True, num_workers=2)
+    batch_size=16, shuffle=True, num_workers=2)
 cifar100_test_dataloader = torch.utils.data.DataLoader(cifar100_test_dataset, \
-    batch_size=128, shuffle=True, num_workers=2)
-
+    batch_size=16, shuffle=True, num_workers=2)
 # model
 model = classifier.Classifier("resnet18", 10)
 model.add_classifier_head(100)
