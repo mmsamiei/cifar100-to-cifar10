81e4667c38423139a279ad4ef84c22b1e985acb2
mmsamiei <mohmahsamiei@gmail.com>
2022-01-27 14:12:43 +0330

    another bug fixed in cifar100

--- experiments/cifar100_to_cifar10.py
+++ experiments/cifar100_to_cifar10.py
@@ -59,7 +59,7 @@ print("Task cifar100:")
 optimizer = torch.optim.SGD(model.parameters(), lr=0.1, momentum=0.9, weight_decay=5e-4)
 scheduler = torch.optim.lr_scheduler.CosineAnnealingLR(optimizer, T_max=cifar10_epochs+cifar100_epochs)
 trainer = supervised_trainer.SupervisedTrainer(cifar100_train_dataloader, \
-    cifar10_test_dataloader, model, optimizer, device, head_num=1, scheduler=scheduler)
+    cifar100_test_dataloader, model, optimizer, device, head_num=1, scheduler=scheduler)
 trainer.run(num_epoch=cifar100_epochs)
 print("Acc of cifar100 : ", tester.test(cifar100_test_dataloader, model, 1, device))
 print("*"*10)
